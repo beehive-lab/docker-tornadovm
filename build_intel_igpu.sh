f7d16a931bf2287c8a4a553e55f387eeaf5fce5e
Juan Fumero <juan.fumero@manchester.ac.uk>
2019-06-20 09:03:40 +0100

    intel GPU tag image and dockerhub name fixed

--- build_intel_igpu.sh
+++ build_intel_igpu.sh
@@ -3,7 +3,7 @@
 IMAGE=tornado-intel-igpu
 docker build --cpuset-cpus="0-7" -t $IMAGE -f Dockerfile-intel-igpu .
 
-TAG=dev
-docker tag tornado-intel-igpu beehivelab/tornado-gpu:$TAG
+TAG=latest
+docker tag tornado-intel-igpu beehivelab/tornado-intel-gpu:$TAG
 
 
--- run_intel.sh
+++ run_intel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-IMAGE=tornado-intel-igpu:latest
+IMAGE=beehivelab/tornado-intel-gpu:latest
 exec docker run -it --device /dev/dri:/dev/dri --rm -v $PWD:/example -w /example "$IMAGE" "$@"
 
