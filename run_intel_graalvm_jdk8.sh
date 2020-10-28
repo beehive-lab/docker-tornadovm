a424da2b1e3727c86232987187b2be7c4b95378a
Juan Fumero <jjfumero@gmail.com>
2020-10-28 10:32:33 +0100

    Fix image docker URLs

--- run_intel_graalvm_jdk8.sh
+++ run_intel_graalvm_jdk8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-IMAGE=tornado-intel-igpu-graalvm-jdk8:latest
+IMAGE=beehivelab/tornado-intel-igpu-graalvm-jdk8:latest
 exec docker run -it --device /dev/dri:/dev/dri --rm -v $PWD:/example -w /example "$IMAGE" "$@"
 
