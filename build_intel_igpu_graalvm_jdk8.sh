6a00c3d2ab441b0ebf43cd3f412fcc5014150281
Juan Fumero <jjfumero@gmail.com>
2020-11-11 16:58:30 +0100

    Docker images for Intel Integrated GPUs updated

--- build_intel_igpu_graalvm_jdk8.sh
+++ build_intel_igpu_graalvm_jdk8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMAGE=tornado-intel-igpu-graalvm-jdk8
-docker build --cpuset-cpus="0-7" -t $IMAGE -f Dockerfile-intel-igpu.graalvm.jdk8 .
+docker build --cpuset-cpus="0-7" -t $IMAGE -f Dockerfile.intel.igpu.graalvm.jdk8 .
 
 TAG=0.8
 docker tag $IMAGE beehivelab/$IMAGE:$TAG
