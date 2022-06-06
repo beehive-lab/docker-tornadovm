a6c966d428d8a8b87e24d258e07f06c10afc749c
Juan Fumero <jjfumero@gmail.com>
2022-06-06 11:15:21 +0200

    Image launch nvidia updated

--- run_nvidia.sh
+++ run_nvidia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-IMAGE=beehivelab/tornado-gpu:latest
+IMAGE=beehivelab/tornadovm-nvidia-openjdk:latest
 exec docker run --runtime=nvidia --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
--- run_nvidia_graalvm-jdk17.sh
+++ run_nvidia_graalvm-jdk17.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-IMAGE=beehivelab/tornado-gpu-graalvm-jdk11:latest
+IMAGE=beehivelab/tornadovn-nvidia-graalvm-jdk17:latest
 exec docker run --runtime=nvidia --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
