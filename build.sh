7134faf976c283bf03446fefd08f1d85e4bc6fde
Juan Fumero <jjfumero@gmail.com>
2022-06-06 08:51:21 +0200

    Build process updated

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-TAG_VERSION=0.13
+TAG_VERSION=0.14-dev
 
 function buildDockerImage() {
     IMAGE=$1
@@ -10,31 +10,31 @@ function buildDockerImage() {
     docker tag $IMAGE beehivelab/$IMAGE:latest
 }
 
-function nvidiaJDK11() {
-    buildDockerImage "tornado-gpu" "dockerFiles/Dockerfile.nvidia"
+function nvidiaJDK17() {
+    buildDockerImage "tornadovm-nvidia-openjdk" "dockerFiles/Dockerfile.nvidia.jdk17"
 }
 
-function nvidiaGraalVMJDK11() {
-    buildDockerImage "tornado-gpu-graalvm-jdk11" "dockerFiles/Dockerfile.nvidia.graalvm.jdk11"
+function nvidiaGraalVMJDK17() {
+    buildDockerImage "tornadovm-nvidia-graalvm" "dockerFiles/Dockerfile.nvidia.graalvm.jdk17"
 }
 
-function intelJDK11() {
-    buildDockerImage "tornado-intel-gpu" "dockerFiles/Dockerfile.intel.igpu"
+function intelJDK17() {
+    buildDockerImage "tornadovm-intel-openjdk" "dockerFiles/Dockerfile.oneapi.intel.jdk17"
 }
 
-function intelGraalVMJDK11() {
-    buildDockerImage "tornado-intel-igpu-graalvm-jdk11" "dockerFiles/Dockerfile.intel.igpu.graalvm.jdk11"
+function intelGraalVMJDK17() {
+    buildDockerImage "tornadovm-intel-graalvm" "dockerFiles/Dockerfile.oneapi.intel.graalvm.jdk17"
 }
 
 function printHelp() {
-    echo "TornadoVM docker build"
-    echo "Options: "
+    echo "TornadoVM Docker Build"
+    echo -e "\nOptions: "
     echo "Builds for NVIDIA GPUs:"
-    echo "       --nvidia-jdk11         (OpenCL): Build Docker Image for NVIDIA GPUs using JDK11"
-    echo "       --nvidia-graalVM-JDK11 (OpenCL): Build Docker Image for NVIDIA GPUs using GraalVM JDK11"
-    echo -e "\nBuilds for Intel Integrated GPUs:"
-    echo "       --igpu-jdk11           (OpenCL): Build Docker Image for Intel Integrated GPUs using JDK11"
-    echo "       --igpu-graalVM-JDK11   (OpenCL): Build Docker Image for Intel Integrated GPUs using GraalVM JDK11"
+    echo "       --nvidia-jdk17         (OpenCL): Build Docker Image for NVIDIA GPUs using JDK11"
+    echo "       --nvidia-graalVM-JDK17 (OpenCL): Build Docker Image for NVIDIA GPUs using GraalVM JDK11"
+    echo -e "\nBuilds for Intel Integrated GPUs, Intel CPUs and FPGAs (Emulation Mode):"
+    echo "       --intel-jdk17          (OpenCL, SPIR-V): Build Docker Image for Intel Integrated GPUs, Intel CPUs, and Intel FPGAs using JDK17"
+    echo "       --intel-graalVM-JDK17  (OpenCL, SPIR-V): Build Docker Image for Intel Integrated GPUs, Intel CPUs, and Intel FPGAs using GraalVM JDK17"
     exit 0
 }
 
@@ -53,20 +53,20 @@ while [[ $# -gt 0 ]]; do
     printHelp
     shift
     ;;
-  --nvidia-jdk11)
-    nvidiaJDK11
+  --nvidia-jdk17)
+    nvidiaJDK17
     shift
     ;;
-  --nvidia-graalVM-JDK11)
-    nvidiaGraalVMJDK11
+  --nvidia-graalVM-JDK17)
+    nvidiaGraalVMJDK17
     shift
     ;;
-  --igpu-jdk11)
-    intelJDK11
+  --intel-jdk17)
+    intelJDK17
     shift
     ;;
-  --igpu-graalVM-JDK11)
-    intelGraalVMJDK11
+  --intel-graalVM-JDK17)
+    intelGraalVMJDK17
     shift
     ;;
   esac
