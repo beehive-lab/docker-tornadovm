d6a166e86cdc78a614aa169ce26e6c4ecc4db623
Juan Fumero <jjfumero@gmail.com>
2022-06-06 13:19:55 +0200

    BuildAll script added

--- buildAll.sh
+++ buildAll.sh
@@ -1,13 +1,38 @@
-## Build for Intel Platforms
+#!/bin/env bash 
 
-./build.sh --intel-jdk17
-./run_intel_openjdk.sh tornado -version
-./run_intel_openjdk.sh tornado --version
-./run_intel_openjdk.sh tornado --threadInfo -cp example/target/example-1.0-SNAPSHOT.jar example.MatrixMultiplication
+platform=$1
+
+if [ -v $1 ];then
+    echo "BUILDING DOCKER IMAGES FOR THE $1 PLATFORM"
+else
+    platform="intel"
+    echo "BUILDING DOCKER IMAGES FOR THE $platform PLATFORM"
+if
+
+if [[ $platform == "intel" ]]
+then
+    ./build.sh --intel-jdk17
+    ./run_intel_openjdk.sh tornado -version
+    ./run_intel_openjdk.sh tornado --version
+    ./run_intel_openjdk.sh tornado --threadInfo -cp example/target/example-1.0-SNAPSHOT.jar example.MatrixMultiplication
 
 
-./build.sh --intel-graalVM-JDK17
-./run_intel_graalvm.sh tornado --version
-./run_intel_graalvm.sh tornado -version
-./run_intel_graalvm.sh tornado --threadInfo -cp example/target/example-1.0-SNAPSHOT.jar example.MatrixMultiplication
+    ./build.sh --intel-graalVM-JDK17
+    ./run_intel_graalvm.sh tornado --version
+    ./run_intel_graalvm.sh tornado -version
+    ./run_intel_graalvm.sh tornado --threadInfo -cp example/target/example-1.0-SNAPSHOT.jar example.MatrixMultiplication
+
+elif [[ $platform == "nvidia" ]]
+then
+    ./build.sh --nvidia-jdk17
+    ./run_nvidia_openjdk.sh tornado -version
+    ./run_nvidia_openjdk.sh tornado --version
+    ./run_nvidia_openjdk.sh tornado --threadInfo -cp example/target/example-1.0-SNAPSHOT.jar example.MatrixMultiplication
+
+
+    ./build.sh --nvidia-graalVM-JDK17
+    ./run_nvidia_graalvm.sh tornado --version
+    ./run_nvidia_graalvm.sh tornado -version
+    ./run_nvidia_graalvm.sh tornado --threadInfo -cp example/target/example-1.0-SNAPSHOT.jar example.MatrixMultiplication
+fi
 
