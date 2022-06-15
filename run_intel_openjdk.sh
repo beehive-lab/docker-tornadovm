7850112f227e3d2511dc300540495b3373a4e24d
Juan Fumero <jjfumero@gmail.com>
2022-06-15 13:33:17 +0200

    Run script for intel platforms fixed

--- run_intel_openjdk.sh
+++ run_intel_openjdk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-IMAGE=tornadovm-intel-openjdk:latest
+IMAGE=beehivelab/tornadovm-intel-openjdk:latest
 exec docker run -it --device /dev/dri:/dev/dri --rm -v $PWD:/example -w /example "$IMAGE" "$@"
 
