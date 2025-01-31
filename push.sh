7ed03240e9daa473e343785c2e9209fc3092801e
Juan Fumero <jjfumero@gmail.com>
2025-01-31 12:15:16 +0100

    Push script

---
+++ push.sh
@@ -0,0 +1,9 @@
+tag=1.0.10
+docker push beehivelab/tornadovm-nvidia-openjdk:$tag
+docker push beehivelab/tornadovm-nvidia-openjdk:latest
+docker push beehivelab/tornadovm-nvidia-graalvm:$tag
+docker push beehivelab/tornadovm-nvidia-graalvm:latest
+docker push beehivelab/tornadovm-intel-openjdk:$tag
+docker push beehivelab/tornadovm-intel-openjdk:latest
+docker push beehivelab/tornadovm-intel-graalvm:$tag
+docker push beehivelab/tornadovm-intel-graalvm:latest
