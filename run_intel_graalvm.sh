7c2770c09f107b6982c0794e26efb198d85b68fd
Thanos Stratikopoulos <tstratiko@gmail.com>
2026-07-14 13:38:19 +0300

    Fix TTY error in Intel run scripts by dropping -t from docker run

--- run_intel_graalvm.sh
+++ run_intel_graalvm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 IMAGE=beehivelab/tornadovm-intel-graalvm:latest
-exec docker run -it --device /dev/dri:/dev/dri --rm -v $PWD:/example -w /example "$IMAGE" "$@"
+exec docker run -i --device /dev/dri:/dev/dri --rm -v $PWD:/example -w /example "$IMAGE" "$@"
 
